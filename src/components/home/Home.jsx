b949228183e82d2d6c78d976f2551760cd15aeca
ZidanAli909 <ahmadzidanali909@gmail.com>
2025-09-17 12:58:28 +0700

    My first commit!

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import Home_about from "./about";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Home_about />
+        </div>
+    );
+}
+
+export default Home
