5190bfab4e625753c8361c0adbf5c82a5c66c7f2
ZidanAli909 <ahmadzidanali909@gmail.com>
2025-09-19 15:05:59 +0700

    Another structure revamp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,41 @@
-import Home_about from "../components/about";
-import Portofolio_stacks from "../components/projects";
 import Navbar from "../components/common/Navbar";
+import "./Home.css";
 
-function Home() {
-    return (
-        <body class="bg-gray-100 w-full">
-            <Navbar />
-            <Home_about />
-            <Portofolio_stacks />
-        </body>
-    );
+import zidanImg from "../assets/img/zidan/passphoto.webp";
+
+// Components
+
+function Home_about() {
+  return (
+    <div class="card card_about flex flex-row max-sm:flex-col">
+      <img
+        class="card_about_img drop-shadow-2xl/15"
+        src={zidanImg}
+        alt="Ahmad Zidan Ali"
+      />
+      <section class="card_about_details">
+        <h1 class="text-3xl font-semibold">About Me</h1>
+        <br />
+        <p>Nama saya Ahmad Zidan Ali</p>
+        <p>Saya mahasiswa Universitas Pembangunan Nasional Veteran Jakarta.</p>
+        <p>Saya mengambil jurusan Teknik Informatika.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+// Main Component (Page)
+
+function Home() {
+  return (
+    <body class="bg-gray-100 w-full">
+      <Navbar />
+      <h1 class="text-6xl text-center m-8">Welcome!</h1>
+      <Home_about />
+    </body>
+  );
+}
+
+// Component exports
+export default Home;
---
+++ src/pages/Pictures.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/common/Navbar"
+
+function Pictures() {
+    return (
+        <body>
+            <Navbar />
+            <h1>Pictures</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </body>
+    )
+}
+
+export default Pictures
